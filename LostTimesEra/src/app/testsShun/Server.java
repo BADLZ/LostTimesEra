1ffba2f90ddff38e0d756772d8df5df10cade8e4
BADLZ <48454150+BADLZ@users.noreply.github.com>
2019-07-15 21:54:59 +0100

    Started to make server

--- LostTimesEra/src/app/testsShun/Server.java
+++ LostTimesEra/src/app/testsShun/Server.java
@@ -1,53 +1,33 @@
 package app.testsShun;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Arrays;
 
-public class Server {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        // don't need to specify a hostname, it will be the current machine
-        ServerSocket ss = new ServerSocket(7777);
-        System.out.println("ServerSocket awaiting connections...");
-        Socket socket = ss.accept(); // blocking call, this will wait until a connection is attempted on this port.
-        System.out.println("Connection from " + socket + "!");
+public class Server extends Thread{
+	
+	private final int port = 5025;
+	private final String address = "255.255.255.255";
+	public void run() {
+		ServerSocket serverSocket = null;
 
-        // get the input stream from the connected socket
-        InputStream inputStream = socket.getInputStream();
-        // create a DataInputStream so we can read data from it.
-        ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+		try {
+			
+			InetAddress addr = InetAddress.getByName(address);
+			
+			serverSocket = new ServerSocket(port,50,addr);
+			System.out.println("Servidor aberto na IP/Porta: " + serverSocket.getInetAddress() 
+			+ " " + port);
+			Socket newSock = null;
 
-        // read the list of messages from the socket
-        Object pt = objectInputStream.readObject();
-        try {
-			Method m = pt.getClass().getDeclaredMethod("getGold");
-			Object invoked = m.invoke(pt);
-			System.out.println(invoked);
-		} catch (NoSuchMethodException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SecurityException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			for (;;) {
+				newSock = serverSocket.accept();
+				HandleConnectionThread a = new HandleConnectionThread(newSock);
+				a.start();
+			}
+		} catch (IOException e) {
+			System.err.println("Excepção no servidor: " + e);
 		}
-        System.out.println(Arrays.toString(pt.getClass().getMethods()));
-
-        System.out.println("Closing sockets.");
-        ss.close();
-        socket.close();
-    }
+	} 
 }
